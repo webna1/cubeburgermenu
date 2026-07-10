3b30b47033408c2df4787248df9a5736bf150324
Fahad Al-mubarak <fahadalmubarak1@outlook.com>
2026-07-10 20:04:48 +0300

    Updates

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'cube-assets-v2'
+
+self.addEventListener('install', e => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url)
+  const isAsset = /\.(jpe?g|png|webp|gif|svg|woff2?|ttf)$/i.test(url.pathname)
+  if (!isAsset) return
+
+  e.respondWith(
+    caches.open(CACHE).then(cache =>
+      cache.match(e.request).then(cached => {
+        if (cached) return cached
+        return fetch(e.request).then(res => {
+          if (res.ok) cache.put(e.request, res.clone())
+          return res
+        })
+      })
+    )
+  )
+})
